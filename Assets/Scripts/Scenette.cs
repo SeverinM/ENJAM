a073e2ecd24f89b23d6e3095664c2632978b9da4
DINO <pifi77@hotmail.fr>
2018-10-06 00:21:27 +0200

    Premier test tres nul , a besoin de plein d'ajout

--- Assets/Scripts/Scenette.cs
+++ Assets/Scripts/Scenette.cs
@@ -4,17 +4,58 @@ using UnityEngine;
 
 public class Scenette : MonoBehaviour {
 
+    //Sequence and input section
+
+
+    //Event section
     public delegate void voidFunc();
     public event voidFunc Sucess;
     public event voidFunc Fail;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [Header("Debug part")]
+    public List<KeyCode> premiereSequence = new List<KeyCode>();
+    public GameObject toucheFXPrefab;
+    public List<Transform> positionForTouche = new List<Transform>();
+    public List<ToucheFX> toucheFXoffCurrentSequence = new List<ToucheFX>();
+
+
+
+    // Use this for initialization
+    void Start () {
+        premiereSequence.Add((KeyCode)Random.Range(97, 122));
+        premiereSequence.Add((KeyCode)Random.Range(97, 122));
+        premiereSequence.Add((KeyCode)Random.Range(97, 122));
+
+        LaunchNextSequence();
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
+
+        int i = 0;
+        foreach (KeyCode kc in premiereSequence)
+        {
+            if (Input.GetKeyDown(kc)) {
+
+                toucheFXoffCurrentSequence[i].getFinish();
+               //JAMAIS FAIRE CA !  premiereSequence.Remove(kc);
+                //toucheFXoffCurrentSequence.Remove(toucheFXoffCurrentSequence[i]);
+            }
+            i++;
+        }
 		
 	}
+
+    void LaunchNextSequence()
+    {
+        int i = 0;
+        foreach (KeyCode kc in premiereSequence)
+        {
+            ToucheFX tf = Instantiate(toucheFXPrefab).GetComponent<ToucheFX>();
+            tf.init(positionForTouche[i++].position, kc.ToString());
+            toucheFXoffCurrentSequence.Add(tf);
+        }
+    }
+
 }
---
+++ Assets/Scripts/ToucheFX.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToucheFX : MonoBehaviour {
+
+    public Text charDisplay;
+
+    private Animator _anima;
+
+    public void Start()
+    {
+        _anima = this.GetComponent<Animator>();
+    }
+
+    public void init(Vector2 worldPosition, string c )
+    {
+        charDisplay.text = c;
+        this.transform.position = worldPosition;
+    }
+
+
+    public void getFinish()
+    {
+        _anima.SetTrigger("success");
+        Debug.Log("I do died.");
+    }
+   //getCall by the animation ! 
+    public void endAnimation()
+    {
+        //destroy himself !!! In reality : touchePool deactivate him and keep him in stock for further use
+    }
+}
